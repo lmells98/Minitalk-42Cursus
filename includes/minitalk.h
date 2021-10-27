d853f98f8b4b18bbab11ee5f3b6ddbc7d00ecd9a
lmells98 <leighton.mells23@gmail.com>
2021-10-27 18:59:45 +1030

    Header File

---
+++ includes/minitalk.h
@@ -0,0 +1,12 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../srcs/ft_printf/includes/ft_printf.h"
+
+#include <stdio.h>
+
+#endif
